83c0e01b1ae3014929fb8cfab63451dd107e51b1
Denis <denis.carlus@hotmail.fr>
2016-11-21 19:36:51 +0100

    Make DOOMful be compilable on Microsoft Windows operating systems

--- Libraries/DOOMful/src/utils/Translation.cpp
+++ Libraries/DOOMful/src/utils/Translation.cpp
@@ -8,19 +8,36 @@
  */
 
 #include <utils/Translation.hpp>
-#include <libintl.h>
-#include <clocale>
 
-namespace Doom {
-    namespace Translation {
-        void Init(const std::string domain, const std::string& path) {
-            setlocale(LC_ALL, "") ;
-            bindtextdomain(domain.c_str(), path.c_str()) ;
-            textdomain(domain.c_str()) ;
-        }
+#ifdef __linux__
+    // Apply translations on GNU/Linux systems.
+    #include <libintl.h>
+    #include <clocale>
 
-        std::string Get(const std::string& str) {
-            return std::string(gettext(str.c_str())) ;
-        }
+    namespace Doom {
+        namespace Translation {
+            void Init(const std::string domain, const std::string& path) {
+                setlocale(LC_ALL, "") ;
+                bindtextdomain(domain.c_str(), path.c_str()) ;
+                textdomain(domain.c_str()) ;
+            }
+
+            std::string Get(const std::string& str) {
+                return std::string(gettext(str.c_str())) ;
+            }
+        } ;
     } ;
-} ;
+
+#else
+    // Do not apply translations on other systems.
+    namespace Doom {
+        namespace Translation {
+            void Init(const std::string, const std::string&) {}
+
+            std::string Get(const std::string& str) {
+                return str ;
+            }
+        } ;
+    } ;
+
+#endif
