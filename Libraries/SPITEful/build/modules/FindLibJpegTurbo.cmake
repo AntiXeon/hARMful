066cda7a368a67e7c1035edc17c0140964f7ed73
dcarlus <bibi09x@live.fr>
2019-01-15 19:04:57 +0100

    Try to fix again CircleCI...

--- Libraries/SPITEful/build/modules/FindLibJpegTurbo.cmake
+++ Libraries/SPITEful/build/modules/FindLibJpegTurbo.cmake
@@ -15,14 +15,21 @@ find_path(JPEGTURBO_INCLUDE_DIR turbojpeg.h
                 /opt/libjpeg-turbo/include
                 NO_DEFAULT_PATH)
 set(JPEGTURBO_NAMES ${JPEGTURBO_NAMES} libturbojpeg turbojpeg)
+set(JPEG_NAMES ${JPEG_NAMES} libjpeg jpeg)
 
 find_library(JPEGTURBO_LIBRARY NAMES ${JPEGTURBO_NAMES}
              PATHS ${JPEGTURBO_INCLUDE_DIR}/../lib
                    ${JPEGTURBO_PATH}/lib64
                    ${JPEGTURBO_PATH}/lib
                    /opt/libjpeg-turbo/lib64
-                   NO_DEFAULT_PATH
-                   NO_CMAKE_SYSTEM_PATH)
+                   NO_DEFAULT_PATH)
+
+find_library(JPEG_LIBRARY NAMES ${JPEG_NAMES}
+            PATHS ${JPEGTURBO_INCLUDE_DIR}/../lib
+                  ${JPEGTURBO_PATH}/lib64
+                  ${JPEGTURBO_PATH}/lib
+                  /opt/libjpeg-turbo/lib64
+                  NO_DEFAULT_PATH)
 
 if (NOT JPEGTURBO_LIBRARY)
 find_library(JPEGTURBO_LIBRARY NAMES ${JPEGTURBO_NAMES}
@@ -32,16 +39,26 @@ find_library(JPEGTURBO_LIBRARY NAMES ${JPEGTURBO_NAMES}
                    ${JPEGTURBO_PATH}/lib64
                    /usr/lib
                    /usr/lib64
-                   NO_DEFAULT_PATH
-                   NO_CMAKE_SYSTEM_PATH)
+                   NO_DEFAULT_PATH)
+
+find_library(JPEG_LIBRARY NAMES ${JPEG_NAMES}
+           PATHS ${JPEGTURBO_INCLUDE_DIR}/../lib
+                 ${JPEGTURBO_INCLUDE_DIR}/../lib64
+                 ${JPEGTURBO_PATH}/lib
+                 ${JPEGTURBO_PATH}/lib64
+                 /usr/lib
+                 /usr/lib64
+                 NO_DEFAULT_PATH)
 endif ()
 
 # handle the QUIETLY and REQUIRED arguments and set JPEG_FOUND to TRUE if
 # all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(JPEGTURBO DEFAULT_MSG JPEGTURBO_LIBRARY JPEGTURBO_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(JPEGTURBO DEFAULT_MSG JPEGTURBO_LIBRARY JPEG_LIBRARY JPEGTURBO_INCLUDE_DIR)
 
 if(JPEGTURBO_FOUND)
   set(JPEGTURBO_LIBRARIES ${JPEGTURBO_LIBRARY})
+  set(JPEG_LIBRARIES ${JPEG_LIBRARY})
 endif()
 
-mark_as_advanced(JPEGTURBO_LIBRARY JPEGTURBO_INCLUDE_DIR )
+mark_as_advanced(JPEGTURBO_LIBRARY JPEGTURBO_INCLUDE_DIR)
+mark_as_advanced(JPEG_LIBRARY JPEGTURBO_INCLUDE_DIR)
