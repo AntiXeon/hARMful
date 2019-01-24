63ae6832928b3b8ba32cf793b7fd6f5a3eb8c537
dcarlus <bibi09x@live.fr>
2019-01-24 22:28:07 +0100

    Add the RenderConfiguration component to the root entity of the scene

--- Libraries/HOPEful/include/scene/Scene.hpp
+++ Libraries/HOPEful/include/scene/Scene.hpp
@@ -2,6 +2,7 @@
 #define __HOPE_SCENE__
 
 #include <scene/Entity.hpp>
+#include <scene/components/RenderConfiguration.hpp>
 
 namespace Hope {
     /**
@@ -14,11 +15,28 @@ namespace Hope {
              */
             Entity m_root ;
 
+            /**
+             * Enable the configuration of the rendering through frame graph
+             * nodes.
+             */
+            RenderConfiguration m_renderConfig ;
+
         public:
+            /**
+             * Create a new Scene instance.
+             */
+            Scene() ;
+
+
             /**
              * Get the root entity of the scene.
              */
             Entity& root() ;
+
+            /**
+             * Set the root of the framegraph.
+             */
+            void setFrameGraphRoot(FrameGraphNode* root) ;
     } ;
 }
 
--- Libraries/HOPEful/src/scene/Scene.cpp
+++ Libraries/HOPEful/src/scene/Scene.cpp
@@ -2,6 +2,14 @@
 
 using namespace Hope ;
 
+Scene::Scene() {
+    m_root.addComponent(&m_renderConfig) ;
+}
+
 Entity& Scene::root() {
     return m_root ;
+}
+
+void Scene::setFrameGraphRoot(FrameGraphNode* root) {
+    m_renderConfig.setFrameGraphRoot(root) ;
 }
