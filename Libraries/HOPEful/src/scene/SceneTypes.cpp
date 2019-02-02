fd60481dcb04cec6a8eddcce745779056126cf81
dcarlus <bibi09x@live.fr>
2019-02-02 10:38:21 +0100

    Define LightType value

--- Libraries/HOPEful/src/scene/SceneTypes.cpp
+++ Libraries/HOPEful/src/scene/SceneTypes.cpp
@@ -4,4 +4,5 @@ namespace Hope {
     ComponentType MeshComponentType =               1 ;
     ComponentType CameraComponentType =             MeshComponentType + 1 ;
     ComponentType RenderConfigurationType =         CameraComponentType + 1 ;
+    ComponentType LightType =                       RenderConfigurationType + 1 ;
 }
