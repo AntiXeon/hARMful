96673211c1d81b53b7d4ede4614c98bca0742229
dcarlus <denis.carlus@hotmail.fr>
2020-03-28 18:31:06 +0100

    Try to understand why CircleCI fails on GLSL2CPP

--- Tools/GLSL2Cpp/src/main.cpp
+++ Tools/GLSL2Cpp/src/main.cpp
@@ -6,23 +6,33 @@
 #include <Paths.hpp>
 
 void createSymLink() {
+std::cout << "enter createSymLink()" << std::endl;
+
     const std::string PathSeparatorStr = std::string(PathSeparator) ;
 
     fs::path currentDir = fs::current_path() ;
     fs::path harmfulDir = currentDir ;
-
+std::cout << "currentDir " << currentDir << std::endl;
     const std::string WantedDirName(ProjectDirName) ;
     std::string stem = harmfulDir.stem().generic_string() ;
+
+std::cout << "stem " << stem << std::endl;
+
     while (harmfulDir.has_parent_path() && (stem != WantedDirName)) {
         harmfulDir = harmfulDir.parent_path() ;
         stem = harmfulDir.stem().generic_string() ;
     }
+std::cout << "stem (2) " << stem << std::endl;
 
 	fs::path libDirectoryPath(harmfulDir.generic_string() + PathSeparator + LibDirectory) ;
 	libDirectoryPath = libDirectoryPath ;
 
+std::cout << "libDirectoryPath " << libDirectoryPath << std::endl;
+
 	fs::path symlibDirectoryPath(SimLinkDirectory) ;
 	symlibDirectoryPath = symlibDirectoryPath ;
+
+std::cout << "symlibDirectoryPath " << symlibDirectoryPath << std::endl;
 
 	if (fs::exists(symlibDirectoryPath)) {
 	    return ;
@@ -32,16 +42,21 @@ void createSymLink() {
 	std::error_code error ;
 	fs::create_directory_symlink(libDirectoryPath, symlibDirectoryPath, error) ;
 
+std::cout << "create_directory_symlink " << std::endl;
+
 	if (error) {
 		auto errorMsg = error.message() ;
 		std::cerr << "ERROR: " << error << errorMsg << std::endl ;
 	}
+
+    std::cout << "exit createSymLink()" << std::endl;
 }
 
 int main() {
     createSymLink() ;
 
     const std::string PathSeparatorStr = std::string(PathSeparator) ;
+    std::cout << "PathSeparatorStr = " << PathSeparatorStr << std::endl;
 
     // Get the shader files.
     auto shaderFiles = FileUtils::GetShaderFiles(ShadersDirectory) ;
