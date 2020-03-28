6e9b85943f2481609a968f1fc840b72c5a756459
dcarlus <denis.carlus@hotmail.fr>
2020-03-28 19:35:55 +0100

    Set minimal versions to older ones for CircleCI Debian...

--- Libraries/SPITEful/build/dependencies.cmake
+++ Libraries/SPITEful/build/dependencies.cmake
@@ -1,6 +1,6 @@
 # Minimal versions of the dependencies.
-SET(LIBJPEGTURBO_REQUIRED_VERSION "2.0.2")
-SET(LIBPNG_REQUIRED_VERSION "1.6.37")
+SET(LIBJPEGTURBO_REQUIRED_VERSION "2.0.1")
+SET(LIBPNG_REQUIRED_VERSION "1.6.35")
 
 
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/modules/")
@@ -14,7 +14,7 @@ if (JPEGTURBO_FOUND AND NOT ${USE_CONAN_LIBS})
     include_directories(${JPEGTURBO_INCLUDE_DIR})
     target_link_libraries(${PROJECT_NAME} ${JPEGTURBO_LIBRARIES})
     target_link_libraries(${PROJECT_NAME} ${JPEG_LIBRARIES})
-elseif(NOT NO_CONAN)
+else()
     set(USE_CONAN_LIBS ON)
     list(APPEND CONAN_LIBS "libjpeg-turbo/${LIBJPEGTURBO_REQUIRED_VERSION}@bincrafters/stable")
     list(APPEND CONAN_LINK CONAN_PKG::libjpeg-turbo)
@@ -25,13 +25,13 @@ find_package(PNG ${LIBPNG_REQUIRED_VERSION} QUIET)
 if (PNG_FOUND AND NOT ${USE_CONAN_LIBS})
     include_directories(${PNG_INCLUDE_DIRS})
     target_link_libraries(${PROJECT_NAME} ${PNG_LIBRARIES})
-elseif(NOT NO_CONAN)
+else()
     set(USE_CONAN_LIBS ON)
     list(APPEND CONAN_LIBS "libpng/${LIBPNG_REQUIRED_VERSION}@bincrafters/stable")
     list(APPEND CONAN_LINK CONAN_PKG::libpng)
 endif()
 
-if (NOT NO_CONAN AND ${USE_CONAN_LIBS})
+if (${USE_CONAN_LIBS})
     conan_cmake_run(
         REQUIRES ${CONAN_LIBS}
         BASIC_SETUP CMAKE_TARGETS
